b3bf787b7942a86087e0f420bcaab1808470961c
JonahT22 <jonahtaylor@att.net>
2021-01-27 09:45:57 -0600

    Create testingLED.py

---
+++ Lab 1/testingLED.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BOARD)
+ledNum = 40
+GPIO.setup(ledNum, GPIO.OUT)
+# starting with frequency 100
+pwm = GPIO.PWM(led, 100)
+# stating with 0, that is off state
+pwm.start(0)
+try:
+    while 1:
+        # duty cycle from 0% to 100%
+        for dc in range(0, 101, 5):
+            pwm.ChangeDutyCycle(dc)
+            time.sleep(0.1)
+        # duty cycle from 100% to 0%
+        for dc in range(100, -1, -5):
+            pwm.ChangeDutyCycle(dc)
+            time.sleep(0.1)
+except KeyboardInterrupt:
+    pwm.stop()
+    GPIO.cleanup()
