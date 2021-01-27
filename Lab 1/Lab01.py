57e7a0d5afa2360b605ffe412cd8e0992bc1ec0a
JonahT22 <jonahtaylor@att.net>
2021-01-27 10:17:50 -0600

    Assigning GPIO pins to inputs/outpus

---
+++ Lab 1/Lab01.py
@@ -0,0 +1,24 @@
+# NOTES:
+# -To get an orange color, set frequency to 100, duty cycle to 90,
+#  send that signal to the red terminal, give green full power
+
+# OUTPUT PORTS
+tl1_Red = 2
+tl1_Yellow_R = 3  # The red terminal of the yellow light
+tl1_Yellow_G = 4  # The green terminal of the yellow light
+tl1_Green = 17
+tl2_Red = 27
+tl2_Yellow_R = 22
+tl2_Yellow_G = 10
+tl2_Green = 9
+
+panel_a = 11
+panel_b = 5
+panel_c = 6
+panel_d = 13
+panel_e = 19
+panel_f = 26
+panel_g = 14
+
+# INPUT PORTS
+tl1_Button = 15
