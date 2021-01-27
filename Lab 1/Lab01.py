08b380e0700d281b70b7a10589cc162176bb51f1
JonahT22 <jonahtaylor@att.net>
2021-01-27 11:02:29 -0600

    Finishing initial setup of variables in lab01.py

--- Lab 1/Lab01.py
+++ Lab 1/Lab01.py
@@ -1,6 +1,6 @@
 import RPi.GPIO as GPIO
 import time
-import segDisplay.py as display
+import segDisplay
 
 # NOTES:
 # -To get an orangeish-yellow color, set frequency to 100, duty cycle to 90,
@@ -20,8 +20,10 @@ tl2_Green = 9
 # (in the order a, b, c, d, e, f, g)
 panelPorts = [11, 5, 6, 13, 19, 26, 14]
 
+
 # INPUT PORTS
 tl1_Button = 15
+
 
 # SETUP
 GPIO.setmode(GPIO.BOARD)
@@ -34,20 +36,12 @@ GPIO.setup(tl2_Yellow_R, GPIO.OUT, initial = 0)
 GPIO.setup(tl2_Yellow_G, GPIO.OUT, initial = 0)
 GPIO.setup(tl2_Green, GPIO.OUT, initial = 0)
 
+# Create pwm outputs with default frequency of 100
+tl1_R_pwm = GPIO.PWM(tl1_Yellow_R, 100)
+tl2_R_pwm = GPIO.PWM(tl2_Yellow_R, 100)
 
-# MAIN
-try:
-    # Testing the 7-segment display
-    display.setDisplayNum(panelPorts, 3)
-    time.sleep(1)
-    display.setDisplayNum(panelPorts, 2)
-    time.sleep(1)
-    display.setDisplayNum(panelPorts, 1)
-    time.sleep(1)
 
-except KeyboardInterrupt:
-    GPIO.cleanup()
-    print("Exiting...")
+
 
 
 
