bb178b2cccfb0d3d66481f46754956a74219d5d9
OsricNagle <osric.nagle@gmail.com>
2021-02-03 10:23:00 -0600

    Updates on function_generator.py

--- Lab 2/function_generator.py
+++ Lab 2/function_generator.py
@@ -4,9 +4,10 @@ import math
 import sin_wave
 
 #declare methods for generating each function
+#move each of these to their own file
 def square_wave():
 
-def triange_wave():
+def triangle_wave():
 
 #Set up board
 GPIO.setmode(GPIO.board)
@@ -20,7 +21,8 @@ GPIO.setup(function_signal, GPIO.OUT, initial = 0)
 #1. Wait for push button input to start program
 
 
-#2. Ask user for: function name (s, t, sin), frequency, max voltage (up to 5V?)
+#2. Ask user for: function name (sq, tr, sin), frequency, max voltage (3 < x < 5V)
+# have three separate prompts for each input field
 
 
 #3. Parse input from user, generate correct function until next button input
