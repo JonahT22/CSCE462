c293ce7509d80c0a5d53a7d00deae70f7993ad92
JonahT22 <jonahtaylor@att.net>
2021-02-12 11:03:58 -0600

    Adding smoothing and input cleanup

--- Lab 3/oscilloscope.py
+++ Lab 3/oscilloscope.py
@@ -25,25 +25,45 @@ input_chan = AnalogIn(mcp, MCP.P0)
 #General structure of program:
 desired_voltage = 1
 sample_average = 0.0
-error_threshold = 0.15
+error_threshold = 0.1
+
+def collectData():
+    sum = 0
+    for i in range(0, 2):
+        sum += input_chan.voltage
+    output = sum / 2.0
+    return output
 
 #Period finding function
 #can change the zero requirement to just any arbitrary voltage value,
 #in case not all functions are centered at zero.
 def find_period():
-    while input_chan.voltage > desired_voltage:
+    """while input_chan.voltage > desired_voltage:
         pass
     while input_chan.voltage < desired_voltage:
         pass                        #do nothing; wait until zero voltage is measured
+    """
+    while collectData() > desired_voltage:
+        pass
+    while collectData() < desired_voltage:
+        pass
     #print("voltage = ", input_chan.voltage)
     time_begin = time.time()        #first zero reached 
     #print(time_begin)
-    #time.sleep(0.04)                #eliminate jitter by sleeping
+    #time.sleep(0.02)                #eliminate jitter by sleeping
     #Loop until next zero voltage value found
+    """
     while input_chan.voltage > desired_voltage:
         pass                        #do nothing
+    #time.sleep(0.02)
     while input_chan.voltage < desired_voltage:
         pass
+    """
+    while collectData() > desired_voltage:
+        pass
+    while collectData() < desired_voltage:
+        pass
+    #time.sleep(0.02)
     #print("voltage = ", input_chan.voltage)
     time_end = time.time()          #second zero reached
     #print(time_end)
@@ -56,43 +76,48 @@ def find_period():
 #def find_max_min(freq):
     
 try:
-    #1. Find frequency (how? - check for first instance of zero, sleep for a time)
-    # The time.sleep() works for eliminating jitter since there is a narrow band of possible
-    # frequencies (1 - 20 Hz).
-    period = find_period()
-    time.sleep(0.125 * period)
-    #sample 10 data points
-    for i in range(1,10):
-        sample_average += input_chan.voltage
-    sample_average /= 10.0
-    print("Period: ", period)
-    print("Average sampled data: ", sample_average)
+    while(True):
+        #1. Find frequency (how? - check for first instance of zero, sleep for a time)
+        # The time.sleep() works for eliminating jitter since there is a narrow band of possible
+        # frequencies (1 - 20 Hz).
+        period = find_period()
+        sleeptime = abs(.125 * period - 0.00225)
+        time.sleep(sleeptime)
+        #sample 10 data pointa
+        sample_average = input_chan.voltage
+        """
+        for i in range(0,2):
+            sample_average += input_chan.voltage
+        sample_average /= 2.0"""
+        #print("Frequency: ", 1.0 / period)
+        #print("Average sampled data: ", sample_average)
 
-    #2. Find max/min voltages (how?)
-    min_volt = 0
-    max_volt = 2
+        #2. Find max/min voltages (how?)
+        min_volt = 0
+        max_volt = 2
 
-    #3. Using previous two, use expected function values at specific intervals to determine
-    # type of function.
-    # specific interval = 1/8 * period
-    # assumption: function is rising to its max value when we make the check. 
-    expected_amplitude_sin =  (max_volt - min_volt) * 0.707 / 2 + (max_volt + min_volt) / 2
-    expected_amplitude_square = max_volt
-    expected_amplitude_tri = .75(max_volt - min_volt) + min_volt
+        #3. Using previous two, use expected function values at specific intervals to determine
+        # type of function.
+        # specific interval = 1/8 * period
+        # assumption: function is rising to its max value when we make the check. 
+        expected_amplitude_sin =  (max_volt - min_volt) * 0.707 / 2 + (max_volt + min_volt) / 2
+        expected_amplitude_square = max_volt
+        expected_amplitude_tri = .75 * (max_volt - min_volt) + min_volt
 
-    #Compare sample sum to expected values
-    if abs(expected_amplitude_sin - sample_average) < error_threshold:
-        #Sin wave likely found
-        print("Found sin wave")
-    elif abs(expected_amplitude_tri - sample_average) < error_threshold:
-        #Tri wave likely found
-        print("Found tri wave")
-    elif abs(expected_amplitude_square - sample_average) < error_threshold:
-        #Square wave likely found
-        print("Found sq wave")
-    else:
-        print("bruh moment - function not identified")
-
+        #Compare sample sum to expected values
+        if abs(expected_amplitude_sin - sample_average) < error_threshold:
+            #Sin wave likely found
+            print("Sin,   ", end='')
+        elif abs(expected_amplitude_tri - sample_average) < error_threshold:
+            #Tri wave likely found
+            print("Tri,   ", end='')
+        elif abs(expected_amplitude_square - sample_average) < error_threshold:
+            #Square wave likely found
+            print("Square ", end='')
+        else:
+            print("bruh   ", end='')
+        
+        print("Frequency = ", 1 / period)
 
 except KeyboardInterrupt:
     print("Exiting...")
