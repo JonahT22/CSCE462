a1eaab6ae369723e2209f6306cb7b67bf26cd832
JonahT22 <jonahtaylor@att.net>
2021-02-12 09:52:03 -0600

    Fixing up frequency measurement

--- Lab 3/oscilloscope.py
+++ Lab 3/oscilloscope.py
@@ -31,33 +31,44 @@ desired_voltage = 1
 #can change the zero requirement to just any arbitrary voltage value,
 #in case not all functions are centered at zero.
 def find_frequency():
+    while input_chan.voltage > desired_voltage:
+        pass
     while input_chan.voltage < desired_voltage:
         pass                        #do nothing; wait until zero voltage is measured
+    print("voltage = ", input_chan.voltage)
     time_begin = time.time()        #first zero reached 
-    time.sleep(0.04)                #eliminate jitter by sleeping
+    print(time_begin)
+    #time.sleep(0.04)                #eliminate jitter by sleeping
     #Loop until next zero voltage value found
     while input_chan.voltage > desired_voltage:
         pass                        #do nothing
+    while input_chan.voltage < desired_voltage:
+        pass
+    print("voltage = ", input_chan.voltage)
     time_end = time.time()          #second zero reached
-    freq = 1.0 / (time_end - time_begin)
+    print(time_end)
+    print("Diff = ", time_end - time_begin)
+    freq = 1.0 / ( (time_end - time_begin))
     return freq
 
 #Max/min voltage finding function
 #def find_max_min(freq):
     
-
+try:
 #1. Find frequency (how? - check for first instance of zero, sleep for a time)
 # The time.sleep() works for eliminating jitter since there is a narrow band of possible
 # frequencies (1 - 20 Hz).
-freq = find_frequency()
-print(freq)
+    freq = find_frequency()
+    print(freq)
 
 #2. Find max/min voltages (how?)
-min_volt = 0
-max_volt = 2
+    min_volt = 0
+    max_volt = 2
 
 #3. Using previous two, use expected function values at specific intervals to determine
 # type of function.
 
+except KeyboardInterrupt:
+    print("Exiting...")
 
 #4. Continuously check for frequency, make sure it's on the same function (tolerance = within 0.5 Hz of defined value)
