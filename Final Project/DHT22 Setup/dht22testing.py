0a29ea71ed2d4795bf05b8e9453b186e687c0f05
JonahT22 <jonahtaylor@att.net>
2021-04-06 21:52:52 -0500

    test plotting

--- Final Project/DHT22 Setup/dht22testing.py
+++ Final Project/DHT22 Setup/dht22testing.py
@@ -1,14 +1,33 @@
 import RPi.GPIO as GPIO
 import Adafruit_DHT
+import numpy as np
+import matplotlib.pyplot as plt
+from time import perf_counter
 
 DHT_SENSOR = Adafruit_DHT.AM2302
 GPIO.setmode(GPIO.BCM)
 DHT_PIN = 18
 
-while True:
-	humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+startTime = perf_counter()
+timevals = np.array()
+humidityvals = np.array()
 
-	if humidity is not None and temperature is not None:
-		print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
-	else:
-		print("Failed to retrieve data from humidity sensor")
+try:
+	while True:
+		humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+
+		if humidity is not None and temperature is not None:
+			print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
+			timevals.append(perf_counter() - startTime)
+			humidityvals.append(humidity)
+		else:
+			print("Failed to retrieve data from humidity sensor")
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    print("Showing output")
+	
+plt.xlabel('Time')
+plt.ylabel('Humidity')
+plt.plot(timevals, humidityvals)
+plt.show()
