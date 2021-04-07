5e4a0b4d7104d0671b1964512e2042a07d33e39e
JonahT22 <jonahtaylor@att.net>
2021-04-06 21:37:56 -0500

    fix typos

--- Final Project/DHT22 Setup/dht22testing.py
+++ Final Project/DHT22 Setup/dht22testing.py
@@ -1,3 +1,4 @@
+import RPi.GPIO as GPIO
 import Adafruit_DHT
 
 DHT_SENSOR = Adafruit_DHT.AM2302
@@ -5,7 +6,7 @@ GPIO.setmode(GPIO.BCM)
 DHT_PIN = 18
 
 while True:
-	humidity, temperature = Adafruit_DHT.read_reatry(DHT_SENSOR, DHT_PIN)
+	humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
 
 	if humidity is not None and temperature is not None:
 		print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
