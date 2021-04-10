e3776990c85df60628795ac2666af9b735b30ccb
JonahT22 <jonahtaylor@att.net>
2021-04-10 12:12:59 -0500

    Test button switch

---
+++ Final Project/pumpcontrol.py
@@ -0,0 +1,31 @@
+# This file handles controlling the relay module
+# Wiring setup:
+#    
+
+
+
+import RPi.GPIO as GPIO
+import time
+
+
+print(GPIO.RPI_INFO)
+
+#Set GPIO pin mode
+GPIO.setmode(GPIO.BOARD)
+
+input1 = 40
+output1 = 36
+GPIO.setup(input1, GPIO.IN, GPIO.PUD_DOWN)
+GPIO.setup(output1, GPIO.OUT, initial = 0)
+
+try:
+	while(True):
+		if(GPIO.input(btn) == 1):
+			print("button on")
+			GPIO.output(output1, GPIO.HIGH)
+		else:
+			print("off")
+			GPIO.output(output1, GPIO.LOW)
+		time.sleep(0.1)
+except KeyboardInterrupt:
+	GPIO.cleanup()
